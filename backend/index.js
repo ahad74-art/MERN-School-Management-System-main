4537d314bf29b5c6855e39c4c16303a78cbeed28
ahad74-art <sabdulahad967@gmial.com>
2026-05-17 20:32:58 +0500

    Update backend with latest features

--- backend/index.js
+++ backend/index.js
@@ -42,8 +42,12 @@ mongoose
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
-    .then(console.log("Connected to MongoDB"))
-    .catch((err) => console.log("NOT CONNECTED TO NETWORK", err))
+   .then(() => {
+    console.log("Connected to MongoDB");
+})
+.catch((err) => {
+    console.log("NOT CONNECTED TO DATABASE", err);
+})
 
 app.use('/', Routes);
 
